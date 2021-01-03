03421f7bbd0eba77f2c6b01d3ef0266d04b25cf3
peinimogu <1131717341@qq.com>
2021-01-03 16:39:38 +0800

    login

--- src/main.js
+++ src/main.js
@@ -3,16 +3,29 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import store from './store'
+
+import less from 'less'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import {getRequest} from './utils/api'
+import {postRequest} from './utils/api'
+import {deleteRequest} from './utils/api'
+import {putRequest} from './utils/api'
 
 Vue.config.productionTip = false
 Vue.use(ElementUI,{ size: 'small', zIndex: 3000 })
+Vue.use(less)
+Vue.prototype.getRequest = getRequest;
+Vue.prototype.postRequest = postRequest;
+Vue.prototype.deleteRequest = deleteRequest;
+Vue.prototype.putRequest = putRequest;
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
   template: '<App/>'
-})
+}).$mount('#app')
